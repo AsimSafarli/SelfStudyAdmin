0dccc6f53cdb595f6eb7bf46fdec770f124b6070
Asim <saffarrli14@gmail.com>
2024-07-07 14:44:40 +0400

    add fix the code

--- src/app/admin/add-item/page.tsx
+++ src/app/admin/add-item/page.tsx
@@ -1,44 +1,9 @@
-"use client"
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
-function AddItemPage() {
-  const [file, setFile] = useState('');
-
-  const handleFileChange = (e:any) => {
-    const selectedFile = e.target.files[0];
-    setFile(selectedFile);
-  };
-
-  const handleSubmit = (e:any) => {
-    e.preventDefault();
-
-    const formData = new FormData();
-    formData.append('video', file);
-
-    axios.post('api', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    })
-    .then(response => {
-      console.log('Video uploaded successfully:', response.data);
-    })
-    .catch(error => {
-      console.error('Error uploading video:', error);
-    });
-  };
+function AddVideo() {
   return (
-    // accept="image/png, image/gif, image/jpeg"
-    <div className="flex h-full">
-   <form onSubmit={handleSubmit}>
-    <div className=''>
-    <input type="file" onChange={handleFileChange} accept="video/*"  />
-    </div>
-      <button type="submit">Upload Video</button>
-    </form>
-  </div>
+    <div>AddVideo</div>
   )
 }
 
-export default AddItemPage
+export default AddVideo
